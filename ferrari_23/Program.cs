84ede60d79b4fef79c9b1582ac35329d575737ac
Ferrari.20190 <ferrari.michele.studente.181@itispaleocapa.it>
2023-02-01 13:50:45 +0100

    //funzione di aggiunta, commenti

--- ferrari_23/Program.cs
+++ ferrari_23/Program.cs
@@ -10,7 +10,58 @@ namespace ferrari_23
     {
         static void Main(string[] args)
         {
-
+            bool continua = false;
+            string[] array = new string[100];
+            int lunghezza = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Selezionare un'opzione:\n1 - Aggiunta di un animale\n2 - Cancellazione di un animale singolo\n3 - Ordinamento dei nomi\n4 - Ricerca di un animale\n5 - Visualizzazione animali ripetuti\n6 - Modifica di un nome\n7 - Visualizzazione di tutti i nomi\n8 - Ricerca del nome più lungo e più corto\n9 - Cancellazione di tutte le occorrenze di un animale\n0 - Uscita dal programma");
+                switch (Console.ReadLine())
+                {
+                    case "1":                                     //aggiunta
+                        Aggiunta(ref lunghezza, array);
+                        break;                                  
+                    case "2":                                     //cancella
+                        break;
+                    case "3":                                     //bubblesort x ordine alfabetico
+                        break;
+                    case "4":                                     //ricerca sequenziale
+                        break;                                   
+                    case "5":                                     //visualizza animali ripetuti
+                        break;
+                    case "6":                                     //modifica nome
+                        break;
+                    case "7":                                     //visualizza array/animali
+                        for (int i = 0; i < lunghezza; i++)
+                        {
+                            Console.Write($"{array[i]} ");
+                        }
+                        break;
+                    case "8":                                     //ricerca nome più lungo e più corto, visualizzazione
+                        break;
+                    case "9":                                     //cancella tutti i nomi uguali
+                        break;
+                    case "0":                                     //uscita
+                        continua = true;
+                        break;
+                };
+                Console.ReadKey();
+            } while (continua == false);
         }
-    }
+        static void Aggiunta(ref int lunghezza, string[] arr)
+        {
+            Console.WriteLine("Inserire animale: ");
+            arr[lunghezza] = Console.ReadLine();
+            lunghezza++;
+        }                       //aggiunta
+        //cancella
+        //bubblesort
+        //ricerca
+        //visualizza ripetuti
+        //modifica nome
+        //visuaizza array
+        //ricerca nome più lungo/corto
+        //cancella nomi uguali
+    }                   
 }
