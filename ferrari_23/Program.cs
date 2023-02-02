94c3d98d7b5448dfb362b73c3041c77d2dfbfb82
Michele Ferrari <Michele@0m3x7.onmicrosoft.com>
2023-02-02 17:26:51 +0100

    //cancella nomi uguali

--- ferrari_23/Program.cs
+++ ferrari_23/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,11 +17,11 @@ namespace ferrari_23
             do
             {
                 Console.Clear();
-                Console.WriteLine("Selezionare un'opzione:\n1 - Aggiunta di un animale\n2 - Cancellazione di un animale singolo\n3 - Ordinamento dei nomi\n4 - Ricerca di un animale\n5 - Visualizzazione animali ripetuti\n6 - Modifica di un nome\n7 - Visualizzazione di tutti i nomi\n8 - Ricerca del nome più lungo e più corto\n9 - Cancellazione di tutte le occorrenze di un animale\n0 - Uscita dal programma");
+                Console.WriteLine("Selezionare un'opzione:\n1 - Aggiunta di un nome\n2 - Cancellazione di un nome singolo\n3 - Ordinamento dei nomi\n4 - Ricerca di un nome\n5 - Visualizzazione animali ripetuti\n6 - Modifica di un nome\n7 - Visualizzazione di tutti i nomi\n8 - Ricerca del nome più lungo e più corto\n9 - Cancellazione di tutte le occorrenze di un nome\n0 - Uscita dal programma");
                 switch (Console.ReadLine())
                 {
                     case "1":                                     //aggiunta
-                        if (lunghezza < 2)                                    //verifica se l'array è pieno
+                        if (lunghezza < 100)                                    //verifica se l'array è pieno
                         {
                             Console.WriteLine("Inserire nome da aggiungere: ");               
                             string aggiunto = Console.ReadLine();
@@ -63,7 +64,7 @@ namespace ferrari_23
                         }
                         break;
                     case "5":                                     //visualizza animali ripetuti
-                        VisualizzaRipetuti(lunghezza, array);
+                        VisualizzaRipetuti(lunghezza, array);                                                   //<---<---<---<---<---<---<---<---
                         break;
                     case "6":                                     //modifica nome
                         Console.WriteLine("Inserire posizione del nome da modificare: ");
@@ -85,6 +86,17 @@ namespace ferrari_23
                     case "8":                                     //ricerca nome più lungo e più corto, visualizzazione
                         break;
                     case "9":                                     //cancella tutti i nomi uguali
+                        if (lunghezza > 0)
+                        {
+                            Console.WriteLine("Inserire nome da cancellare: ");
+                            string cancellato = Console.ReadLine();
+                            CancellaUguali(ref lunghezza, array, cancellato);           //chiamata della funzione di cancellazione
+                            Console.WriteLine("Il nome è stato cancellato");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Impossibile cancellare nome, array vuoto");
+                        }
                         break;
                     case "0":                                     //uscita
                         continua = true;
@@ -100,16 +112,16 @@ namespace ferrari_23
         }            //aggiunta
         static void Cancella(ref int lunghezza, string[] arr, string can)
         {
-            for (int i = 0; i < lunghezza; i++)            //ciclo che verifica se array[i] == numero per ogni valore da 0 a indice
+            for (int i = 0; i < lunghezza; i++)
             {
-                if (arr[i] == can)                 //condizione che verifica se array[i] == numero
+                if (arr[i] == can) 
                 {
-                    for (; i < lunghezza - 1; i++)         //ciclo che sostituisce ad ogni array[i] il valore successivo nell'array
+                    for (; i < lunghezza - 1; i++) 
                     {
-                        arr[i] = arr[i + 1];        //assegnazione del valore array[i + 1] ad array[i]
+                        arr[i] = arr[i + 1];
                     }
-                    lunghezza--;                           //decremento dell'indice
-                    break;                              //termina il ciclo
+                    lunghezza--;
+                    break;
                 }
             }
         }            //cancella
@@ -167,6 +179,21 @@ namespace ferrari_23
             }
         }                                //visualizza array
                                                                                         //ricerca nome più lungo/corto
-                                                                                        //cancella nomi uguali
+        static void CancellaUguali(ref int lunghezza, string[] arr, string can)
+        {
+            for (int i = 0; i < lunghezza; i++)
+            {
+                if (arr[i] == can) 
+                {
+                    for (int j = i; j < lunghezza - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    arr[lunghezza] = null;
+                    i--;
+                    lunghezza--;
+                }
+            }
+        }      //cancella nomi uguali
     }                   
 }
