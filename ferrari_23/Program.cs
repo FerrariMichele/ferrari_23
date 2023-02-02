91f3a78421abd332d880055f547c9f60448ce0a7
Michele Ferrari <Michele@0m3x7.onmicrosoft.com>
2023-02-02 16:07:53 +0100

    //aggiunta della funzione di cancellazione

--- ferrari_23/Program.cs
+++ ferrari_23/Program.cs
@@ -20,20 +20,30 @@ namespace ferrari_23
                 switch (Console.ReadLine())
                 {
                     case "1":                                     //aggiunta
-                        if (lunghezza < 2)
+                        if (lunghezza < 2)                                    //verifica se l'array è pieno
                         {
-                            Console.WriteLine("Inserire animale: ");
+                            Console.WriteLine("Inserire nome da aggiungere: ");               
                             string aggiunto = Console.ReadLine();
-                            Aggiunta(ref lunghezza, array, aggiunto);
-                            Console.WriteLine("Il numero è stato inserito");
+                            Aggiunta(ref lunghezza, array, aggiunto);           //chiamata della funzione di aggiunta
+                            Console.WriteLine("Il nome è stato inserito");
                         }
                         else
                         {
-                            Console.WriteLine("Impossibile inserire numero, array pieno");
+                            Console.WriteLine("Impossibile inserire nome, array pieno");
                         }
                         break;
                     case "2":                                     //cancella
-                        Cancella();
+                        if (lunghezza > 0)
+                        {
+                            Console.WriteLine("Inserire nome da cancellare: ");
+                            string cancellato = Console.ReadLine();
+                            Cancella(ref lunghezza, array, cancellato);           //chiamata della funzione di cancellazione
+                            Console.WriteLine("Il nome è stato cancellato");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Impossibile cancellare nome, array vuoto");
+                        }
                         break;
                     case "3":                                     //bubblesort x ordine alfabetico
                         break;
@@ -61,23 +71,34 @@ namespace ferrari_23
         {
                 arr[lunghezza] = agg;
                 lunghezza++;
-        }                       //aggiunta
-        static void Cancella()
+        }            //aggiunta
+        static void Cancella(ref int lunghezza, string[] arr, string can)
         {
-
-        }                                                      //cancella
-        //bubblesort
-        //ricerca
-        //visualizza ripetuti
-        //modifica nome
-        static void Visualizza(string[] arr, int lun)                               //visuaizza array
+            for (int i = 0; i < lunghezza; i++)            //ciclo che verifica se array[i] == numero per ogni valore da 0 a indice
+            {
+                if (arr[i] == can)                 //condizione che verifica se array[i] == numero
+                {
+                    for (; i < lunghezza - 1; i++)         //ciclo che sostituisce ad ogni array[i] il valore successivo nell'array
+                    {
+                        arr[i] = arr[i + 1];        //assegnazione del valore array[i + 1] ad array[i]
+                    }
+                    lunghezza--;                           //decremento dell'indice
+                    break;                              //termina il ciclo
+                }
+            }
+        }            //cancella
+                                                                                        //bubblesort
+                                                                                        //ricerca
+                                                                                        //visualizza ripetuti
+                                                                                        //modifica nome
+        static void Visualizza(string[] arr, int lun)                                   //visuaizza array
         {
             for (int i = 0; i < lun; i++)
             {
                 Console.Write($"{arr[i]} ");
             }
         }
-        //ricerca nome più lungo/corto
-        //cancella nomi uguali
+                                                                                        //ricerca nome più lungo/corto
+                                                                                        //cancella nomi uguali
     }                   
 }
